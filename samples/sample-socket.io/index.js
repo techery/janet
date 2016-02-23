c3ca87d82d83538b17038833b150b04bd89dc0ca
dirong <alex.vl.kozlov@gmail.com>
2016-02-23 10:58:53 +0200

    Improved thread and leak safety for AsyncActionSynchronizer

--- samples/sample-socket.io/index.js
+++ samples/sample-socket.io/index.js
@@ -4,7 +4,7 @@ io.on('connection', function(socket){
 	console.log("somebody connected");
   socket.on('test', function(data){
   	console.log("test: " + JSON.stringify(data));
-//  	socket.emit('test', {id: data.id, status: 'success'});
+  	socket.emit('test', {id: data.id, status: 'success'});
   	io.emit('test2', data.data);
   });
 });
