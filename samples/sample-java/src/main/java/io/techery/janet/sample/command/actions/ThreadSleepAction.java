6ab877b88d30f1cf1133e59414951e12a13ae73e
Aleksey Malevaniy <a.malevaniy@gmail.com>
2016-04-20 17:01:32 +0300

    Make CommandActionBase more strict for cancellation

--- samples/sample-java/src/main/java/io/techery/janet/sample/command/actions/ThreadSleepAction.java
+++ samples/sample-java/src/main/java/io/techery/janet/sample/command/actions/ThreadSleepAction.java
@@ -6,27 +6,22 @@ import io.techery.janet.command.annotations.CommandAction;
 @CommandAction
 public class ThreadSleepAction extends CommandActionBase<String> {
 
-    public final static long DURATION = 10 * 1000;
-
-    private boolean cancel;
+    public final static long DURATION = 10L * 1000L;
 
     @Override protected void run(CommandCallback<String> callback) {
         new Thread(() -> {
             int seconds = 0;
-            while (!cancel) {
+            while (!isCanceled()) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                     callback.onFail(e);
                 }
-                callback.onProgress((int) ((++seconds * 100) / (DURATION / 1000)));
+                callback.onProgress((++seconds * 100) / ((int) (DURATION / 1000L)));
             }
             callback.onSuccess("FINISHED");
         }).start();
     }
 
-    @Override public void cancel() {
-        cancel = true;
-    }
 }
