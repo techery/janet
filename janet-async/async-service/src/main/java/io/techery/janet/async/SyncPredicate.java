5ce29b49f348358d46555f7bd19f6ae50db396af
Aleksey Malevaniy <a.malevaniy@gmail.com>
2016-04-21 18:03:08 +0300

    Mass restructuing: remove all additional services and samples

--- janet-async/async-service/src/main/java/io/techery/janet/async/SyncPredicate.java
+++
@@ -1,6 +0,0 @@
-package io.techery.janet.async;
-
-public interface SyncPredicate<T, R> {
-
-    boolean isResponse(T requestAction, R response);
-}
--- samples/sample-java/src/main/java/io/techery/janet/sample/command/actions/WrongAction.java
+++
@@ -1,7 +0,0 @@
-package io.techery.janet.sample.command.actions;
-
-import io.techery.janet.command.annotations.CommandAction;
-
-@CommandAction
-public class WrongAction {
-}
